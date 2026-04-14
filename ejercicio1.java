27cb75d6f64ccce099fdd1d66df22d9e93609802
Pedro Marcos <pedromarcosmangas2006@gmail.com>
2026-04-14 18:10:28 +0200

    Version 2

--- ejercicio1.java
+++ ejercicio1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class ejercicio1 {
+    // Versión 1
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] numeros = new int[5];
@@ -14,5 +15,16 @@ public class ejercicio1 {
         for (int n : numeros) {
             System.out.print(n + " ");
         }
+
+        int suma = 0;
+        for (int n : numeros) {
+            suma += n;
+        }
+
+        double media = (double) suma / numeros.length;
+
+        System.out.println("\nSuma: " + suma);
+        System.out.println("Media: " + media);
     }
+
 }
